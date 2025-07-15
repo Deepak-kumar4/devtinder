5de3cc6c61695ef43b1e604ca94553598fbced9c
Deepak Kumar <deepakobc933@gmail.com>
2025-07-16 00:20:47 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -7,15 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');    
 });
 
-app.post('/data', (req, res) => {
-  res.json({ message: 'Data received' });   
-});
-app.put('/data', (req, res) => {    
-    res.json({ message: 'Data updated' });
+app.get('/about/:id/:name/:password', (req, res) => {
+    console.log(req.params);
+    res.send(`ID: ${req.params.id}, Name: ${req.params.name}, Password: ${req.params.password}`);
+   
     });
-app.delete('/data', (req, res) => {    
-    res.json({ message: 'Data deleted' });
-}); 
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
