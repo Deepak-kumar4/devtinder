54674e7a4bc31212cf32c8fd7cfed24a2c37d103
Deepak Kumar <deepakobc933@gmail.com>
2025-07-15 23:59:45 +0530

    created,updated , deleted

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express= require('express');
 const app = express(); 
 
+
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World!');    
 });
 
-app.get("/test",(req,res)=>{
-    res.send("this is test")
-})
+app.post('/data', (req, res) => {
+  res.json({ message: 'Data received' });   
+});
+app.put('/data', (req, res) => {    
+    res.json({ message: 'Data updated' });
+    });
+app.delete('/data', (req, res) => {    
+    res.json({ message: 'Data deleted' });
+}); 
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
